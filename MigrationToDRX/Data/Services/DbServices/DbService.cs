cdcd92aa09be69a11d325c2c0be9cce4fb39ccc6
Олег Саевский <goattyie@gmail.com>
2025-12-08 08:51:14 +0300

    Добавлена базовая архитектура для миграции из бд

---
+++ MigrationToDRX/Data/Services/DbServices/DbService.cs
@@ -0,0 +1,21 @@
+﻿using Dapper;
+using System.Data.Common;
+
+namespace MigrationToDRX.Data.Services.DbServices
+{
+    public abstract class DbService : IAsyncDisposable
+    {
+        protected DbConnection SqlConnection { get; set; }
+
+        public DbService(DbConnection connection)
+        {
+            SqlConnection = connection;
+        }
+
+        public virtual async Task<IEnumerable<dynamic>> ReadTableAsync(string tableName) => await SqlConnection.QueryAsync($"SELECT * FROM {tableName};");
+
+        public abstract Task<IEnumerable<string>> GetTablesAsync();
+
+        public virtual async ValueTask DisposeAsync() => await SqlConnection.DisposeAsync();
+    }
+}
---
+++ MigrationToDRX/Data/Services/DbServices/MssqlService.cs
@@ -0,0 +1,14 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace MigrationToDRX.Data.Services.DbServices
+{
+    public class MssqlService : DbService
+    {
+        public MssqlService(SqlConnection connection) : base(connection)
+        {
+        }
+
+        public override async Task<IEnumerable<string>> GetTablesAsync() => await SqlConnection.QueryAsync<string>("SELECT TABLE_NAME AS TableName FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'");
+    }
+}
